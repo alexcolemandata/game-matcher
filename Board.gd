9f7824c06329242d7d0ae95a3c4d578c5534c5a4
Alex Coleman <coleman.g.alex@gmail.com>
2025-03-03 20:45:56 +1100

    fix input delay for 'just_pressed' due to keyrepeat logic

--- Board.gd
+++ Board.gd
@@ -97,6 +97,15 @@ var num_steps: int = 0
 const MAX_STEPS: int = 8
 func _process(delta) -> void:
 	if (
+		Input.is_action_just_pressed("down")
+		or Input.is_action_just_pressed("up")
+		or Input.is_action_just_pressed("left")
+		or Input.is_action_just_pressed(("right"))
+	):
+		input_handler(Input)
+		num_steps = 0
+		
+	elif (
 		Input.is_action_pressed("down") 
 		or Input.is_action_pressed("up") 
 		or Input.is_action_pressed("left") 
